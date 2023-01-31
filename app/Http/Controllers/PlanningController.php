9a92bd512e4100870c573613dc5c297535e4a9c9
msczk <maxime43200@gmail.com>
2023-01-31 20:27:34 +0100

    Add "Auth" middleware to controllers

--- app/Http/Controllers/PlanningController.php
+++ app/Http/Controllers/PlanningController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PlanningController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 }
